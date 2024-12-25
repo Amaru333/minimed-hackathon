8f1941ff8fabd6881e7032ca4b26e8e140533f1d
Amaru333 <amrutesharun0599@gmail.com>
2024-12-25 11:31:28 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false, net: false, tls: false };
-    return config;
-  },
+  // webpack: (config) => {
+  //   config.resolve.fallback = { fs: false, net: false, tls: false };
+  //   return config;
+  // },
   images: {
     remotePatterns: [
       {
